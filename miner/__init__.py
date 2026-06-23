74f8f7336feaf164483ac917aadfc3dac3b30e69
Ogban Ugot <ogbanugot@gmail.com>
2026-06-23 16:20:47 +0100

    Export ontology context miner package

--- miner/__init__.py
+++ miner/__init__.py
@@ -1,4 +1,16 @@
 from .section_context_v1 import SectionContextV1Config, SectionContextV1Runner
+from .ontology_context_v1 import (
+    OntologyContextV1Config,
+    OntologyContextV1Miner,
+    OntologyContextV1Runner,
+    OntologyContextV1Validator,
+)
 
-__all__ = ["SectionContextV1Config", "SectionContextV1Runner"]
-
+__all__ = [
+    "SectionContextV1Config",
+    "SectionContextV1Runner",
+    "OntologyContextV1Config",
+    "OntologyContextV1Miner",
+    "OntologyContextV1Runner",
+    "OntologyContextV1Validator",
+]
